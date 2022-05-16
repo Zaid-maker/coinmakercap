048bff0a5fcb4e973f59179019c3343ceedfc45a
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-05-16 20:22:55 +0500

    hmmm

--- components/cmc-table/cmcTableRow.js
+++ components/cmc-table/cmcTableRow.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
-//import More from '../../assets/svg/more'
-//import Star from '../../assets/svg/star'
-//import CoinNameRow from '../coinNameRow'
+import More from '../../assets/svg/more'
+import Star from '../../assets/svg/star'
+import CoinNameRow from '../cmc-table/coinNameRow'
 //import Rate from './rate'
 import { useRouter } from 'next/router'
 
@@ -24,7 +24,42 @@ const CMCtableRow = ({
   volumeCryptoValue = '---',
   circulatingSupply = '---',
 }) => {
-  return <div className={styles.tableRow}>CMCtableRow</div>
+  const graphImages = []
+
+  const getRandomGraph = () => {}
+
+  const router = useRouter()
+
+  const viewCoinDetails = () => {}
+
+  const viewPrice = () => {}
+
+  const formatNum = (num) => {
+    return Number(num.toFixed(2)).toLocaleString()
+  }
+
+  return (
+    <tbody className={styles.tableRow}>
+      <tr>
+        <td>
+          <Star />
+        </td>
+      </tr>
+      <td>{starNum}</td>
+
+      {coinIcon && coinIcon ? (
+        <td className="cursor-pointer">
+          <CoinNameRow
+            name={coinName}
+            icon={coinIcon}
+            clicked={viewCoinDetails}
+          />
+        </td>
+      ) : (
+        <></>
+      )}
+    </tbody>
+  )
 }
 
 export default CMCtableRow
