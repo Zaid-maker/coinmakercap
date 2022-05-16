5e16aeb7dc916236a6eccfc8fa94c8d62c709d3b
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-05-16 22:04:16 +0500

    ..

--- components/cmc-table/cmcTableRow.js
+++ components/cmc-table/cmcTableRow.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import More from '../../assets/svg/more'
 import Star from '../../assets/svg/star'
 import CoinNameRow from '../cmc-table/coinNameRow'
-//import Rate from './rate'
+import Rate from './Rate'
 import { useRouter } from 'next/router'
 
 const styles = {
@@ -24,15 +24,39 @@ const CMCtableRow = ({
   volumeCryptoValue = '---',
   circulatingSupply = '---',
 }) => {
-  const graphImages = []
+  const graphImages = [
+    'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/52.svg',
+    'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/1.svg',
+    'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/825.svg',
+    'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/3408.svg',
+    'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/5426.svg',
+    'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/7129.svg',
+    'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/3957.svg',
+    'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/328.svg',
+    'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/2416.svg',
+    'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/1765.svg',
+    'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/2099.svg',
+    'https://s3.coinmarketcap.com/generated/sparklines/web/7d/2781/7653.svg',
+  ]
 
-  const getRandomGraph = () => {}
+  const getRandomGraph = () => {
+    const rndInt = Math.floor(Math.random() * 10) + 1
+    return graphImages[rndInt]
+  }
 
   const router = useRouter()
 
-  const viewCoinDetails = () => {}
+  const viewCoinDetails = () => {
+    router.push(
+      `/currencies/info?symbol=${coinSymbol}&coin=${coinName}&price=${price}`,
+    )
+  }
 
-  const viewPrice = () => {}
+  const viewPrice = () => {
+    router.push(
+      `/currencies/price?symbol=${coinSymbol}&coin=${coinName}&price=${price}`,
+    )
+  }
 
   const formatNum = (num) => {
     return Number(num.toFixed(2)).toLocaleString()
@@ -58,6 +82,45 @@ const CMCtableRow = ({
       ) : (
         <></>
       )}
+
+      <td className="cursor-pointer" onClick={viewPrice}>
+        <p>${formatNum(price)}</p>
+      </td>
+      <td>
+        <Rate isIncrement={hRateIsIncrement} rate={`${formatNum(hRate)}%`} />
+      </td>
+      <td>
+        <Rate isIncrement={dRateIsIncrement} rate={`${formatNum(dRate)}%`} />
+      </td>
+
+      <td>
+        <div>
+          <p>${formatNum(marketCapValue)}</p>
+        </div>
+      </td>
+
+      <td>
+        <div>
+          <p>${formatNum(volumeValue)}</p>
+          <p className="text-gray-600">
+            ${formatNum(volumeCryptoValue)} {coinSymbol}
+          </p>
+        </div>
+      </td>
+
+      <td>
+        <div>
+          <p>{formatNum(circulatingSupply)}</p>
+        </div>
+      </td>
+
+      <td>
+        <Image src={getRandomGraph()} width={150} height={60} alt="" />
+      </td>
+
+      <td>
+        <More />
+      </td>
     </tbody>
   )
 }
